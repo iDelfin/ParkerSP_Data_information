4a78825c57909c8efbc8431e9fefee06f72edc7d
iDelfin <i.d.g2000@hotmail.com>
2026-07-29 10:34:45 -0600

    Update message format in check_update functionsdfghj

--- dateUpdateCheck.py
+++ dateUpdateCheck.py
@@ -6,7 +6,7 @@ import json
 def check_update(all_data_date_list):
     msg_str =''
     for info_mission in all_data_date_list.keys():
-        msg_str += (info_mission + f" : {all_data_date_list[info_mission]}\n")
+        msg_str += (info_mission + f" : {all_data_date_list[info_mission]} !!!\n")
     requests.post('https://ntfy.sh/PSP_SWP_SPI_SF0A_L3_MOM_dolphin', f'NEW INFO\n{msg_str}')
     with open('lastUpdate.json', 'w', encoding='utf-8') as f:
         json.dump(all_data_date_list, f, ensure_ascii=False, indent=4)
